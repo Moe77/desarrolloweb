bab41fdeb2412c02419c46a45bc32f8a7e3ff362
usuario <usuario@10.0.2.15>
2014-02-04 18:58:00 +0100

    Segundo Commit

---
+++ PruebaGit/src/pruebaGitPaquete/Prueba.java
@@ -0,0 +1,13 @@
+package pruebaGitPaquete;
+
+public class Prueba {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ola ke ase");
+	}
+
+}
